9ea811726e7f3b3a43e13112243dcf585e950a85
Antoine <a.deyris85@gmail.com>
2023-04-14 15:29:29 +0200

    l'api en légende

---
+++ api/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`API lancée sur http://localhost:${port}`);
+})
+
+app.get('/api/arrets', async (req, res) => {
+
+    try {
+        const response = await fetch('https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_tan-arrets&q=&rows=10000');
+        const data = await response.json();
+
+        res.json(data);
+    }
+    
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Erreur lors de la récupération des données' });
+    }
+
+});
+
+app.get('/api/users/:id', (req, res) => {
+    // Récupération de l'utilisateur avec l'ID spécifié dans la requête
+    const userId = req.params.id;
+    const user = { id: userId, name: 'Alice' };
+
+    res.json(user);
+});
