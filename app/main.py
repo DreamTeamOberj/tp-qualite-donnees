0dfa317eefeea7f0f4b3d41eb1361e63178d372b
QuentinSama <quent1c@orange.fr>
2023-05-25 23:49:46 +0200

    resolve merge conflict

--- app/main.py
+++ app/main.py
@@ -20,19 +20,47 @@ def index():
     arrets = requests.get("http://localhost:3000/api/arret")
     arrets = arrets.json()
     circuits = requests.get("http://localhost:3000/api/circuit")
-    circuits = circuits.json()
+    circuits = circuits.json(   )
 
     for circuit in circuits:
         ligne = circuit["coordinates"]
         folium.PolyLine(ligne, color=circuit["couleur"]).add_to(line_cluster)
 
     for arret in arrets:
-        stop_name = arret["nom"]
+        #ligne_name = arret["id"]
+        enfants = arret["enfants"]
+
         # Coordonnées
         lat = arret["latitude"]
         lon = arret["longitude"]
-        icon = folium.Icon(icon='bus', prefix='fa')
-        popup = "<div>Ligne : test<br><b>"+stop_name+"</b><br>Accèssible PMR</div>"
+
+        if type in circuits == "Bus":
+            icon = folium.Icon(icon='train', prefix='fa')
+        elif type in circuits == "Tram":
+            icon = folium.Icon(icon='train-tram', prefix='fa')
+        else :
+            icon = folium.Icon(icon='ship', prefix='fa')
+
+        html = ""
+
+        for enfant in enfants:
+            stop_name = arret["nom"]
+            nom = arret["nom"]
+            acces_handicape = enfant["acces_handicape"]
+
+            html += '''<br>Ligne = '''+stop_name+'''<br>
+            Nom arrêt = '''+nom+'''<br>
+            '''
+
+            if acces_handicape == True:
+                html += "Places handicapées : 1"
+            else:
+                html += "Places handicapées : 2"
+
+        iframe = folium.IFrame(html, width=200, height=200)
+
+        popup = folium.Popup(iframe)
+
         folium.Marker(
             [lat, lon], popup=popup, tooltip=stop_name, icon=icon
         ).add_to(marker_cluster)
